37cd1c00e047bc3554efb05caceebfd989ee8401
Tom Dietel <tom@dietel.net>
2017-06-01 10:55:12 +0200

    add env.sh to set up enironment variables (must be edited)

---
+++ env.sh
@@ -0,0 +1,12 @@
+# Example script to set up the environment for Pythia and ROOT
+#
+# You must edit this file to reflect the locations of your 
+# software packages.
+#
+# Source this file with 
+#   . env.sh
+# 
+export PYTHIA8=/Users/tom/sw/src/pythia8226
+export PYTHIA8DATA=$PYTHIA8/share/Pythia8/xmldoc
+
+. /Users/tom/sw/build/bin/thisroot.sh
