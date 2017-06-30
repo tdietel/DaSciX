ca10fe219ec5eb087a9bbe8d8695f98762fea7c8
Tom Dietel <tom@dietel.net>
2017-06-30 16:16:09 +0200

    remove build directory with binary code

--- Detector/build/CMakeFiles/E02.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/E02"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/E02.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
